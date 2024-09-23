de7b4cdc0b47510c3f9ce868cd4b8716f797b36d
Kartikay786 <rohit2141451519@emil>
2024-09-23 21:37:37 +0530

    reslove netlify error

--- vite.config.js
+++ vite.config.js
@@ -1,12 +1,12 @@
+
+
+// https://vitejs.dev/config/
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vitejs.dev/config/
-export default {
-  build: {
-    rollupOptions: {
-      external: ['react-rating-stars-component']
-    }
-  }
-}
+export default defineConfig({
+  plugins: [react()],
+})
+
+
 
